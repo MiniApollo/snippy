cf5a64118561e5d3c351c141ff0614357e6ddf50
Mark Surmann <surmannmark@gmail.com>
2025-10-23 21:37:37 +0200

    snipEngine: Clean up the code and add todos

--- snipEngine.el
+++ snipEngine.el
@@ -1,3 +1,10 @@
+
+;; TODOS
+;; - Complete at cursor
+;; - Per major mode
+;; - cursor jumps
+
+
 ;;; Beolvasni a current mode snippetjeit, mikor belépünk
 ;; 1. Get current mode
 ;; (message "%s" major-mode)
@@ -9,7 +16,7 @@
 (setq test-snippets (json-read-file "~/Projects/emacsPackages/snipEngine/test.json"))
 ;; (message "%s" test-snippets)
 
-(setq inputString "st")
+(setq inputString "fora")
 (setq c-snippets (json-read-file "~/Projects/emacsPackages/snipEngine/friendly-snippets/snippets/c/c.json"))
 
 ;; 3. Lecserélni a szót a snippetre
@@ -34,9 +41,18 @@
 ;; (message "%s" result)
 ;; (message "%s" (assoc 'body result))
 
-(setq snippet (cdr (assoc 'body result)))
+(setq snippet (cdr (assoc 'body result))) ;; Levágja az elejét és csak az értéket veszi (key . value) -> value
 
-(dolist (current-line snippet)
-  ;; (message "%s" current-line)
-  (insert (format "%s\n" current-line))
+;; (dolist (current-line snippet)
+;;   ;; (message "%s" current-line)
+;;   (insert (format "%s\n" current-line))
+;; )
+
+(defun insertSnippet (snippet)
+  "Insert snippet at cursor point"
+  (interactive)
+  (dolist (current-line snippet)
+	;; (message "%s" current-line)
+	(insert (format "%s\n" current-line))
+	)
 )
