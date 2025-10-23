9fbe35d6dd5ac4f9aa07c68bb436f9825308b987
Mark Surmann <surmannmark@gmail.com>
2025-10-23 18:56:01 +0200

    SnipEngine: Add basic WIP read loop to find prefix

--- snipEngine.el
+++ snipEngine.el
@@ -3,17 +3,40 @@
 ;;; Beolvasni a current mode snippetjeit, mikor belépünk
 
 ;; 1. Get current mode
-(message "%s" major-mode)
+;; (message "%s" major-mode)
 
 ;; 2. Read JSON from this locaiton into var
-;; Read file
-(message "%s" (json-read-file "~/Projects/emacsPackages/snipEngine/test.json"))
 
-(message "%s" (json-read-file "~/Projects/emacsPackages/snipEngine/test.json"))
+(setq json-object-type 'alist) ;; To set type default is alist
 
-;; Parse json
-;; (json-parse-string hello)
+;; (message "%s" (json-read-file "~/Projects/emacsPackages/snipEngine/test.json"))
+(setq test-snippets (json-read-file "~/Projects/emacsPackages/snipEngine/test.json"))
+
+;; (message "%s" test-snippets)
+
+(setq inputString "st")
+
+;; Loop over list
+(let (result)
+  (dolist (current-alist test-snippets result)
+	 (setq result (string-equal inputString (cdr (assoc 'prefix current-alist))) ;; assoc kell alist-get el nem megy
+			)
+	;; (message "Alist: %s" (string-equal inputString (cdr (assoc 'prefix current-alist))) ;; assoc kell alist-get el nem megy
+			 ;; )
+	)
+  )
+(message "%s" result)
+
+;; (setq c-snippets (json-read-file "~/Projects/emacsPackages/snipEngine/friendly-snippets/snippets/c/c.json"))
 
 ;; Function amikor szeretnénk hogy expand legyen
 
 ;; 3. Lecserélni a szót a snippetre
+;; Search by prefix value
+;; Get the body from the object
+;; Expand snippet
+
+;; Gagyi megoldás:
+;; Get prefix name from completion.
+;; Linear search, get object names and check for a prefix match
+;; Get the then get the body and expand
