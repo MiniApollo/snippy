bde925303123a1e94431cfbec3eba3c0f8179619
Mark Surmann <surmannmark@gmail.com>
2026-01-28 21:04:04 +0100

    Snippy: Add vscode engine check

--- snippy.el
+++ snippy.el
@@ -15,22 +15,30 @@
 ;; Only set vars for this package not change it globally
 
 ;;(setq json-object-type 'alist) ;; To set type default is alist
-(setq json-array-type 'list) ;; Default is vector e.g [Hello there my name is]
+(setq-local json-array-type 'list) ;; Default is vector e.g [Hello there my name is]
 
 (defvar snippy/package-json-location "./friendly-snippets/package.json" "Location of the package.json file")
-(setq snippy/package-json-content (json-read-file snippy/package-json-location))
+(defvar snippy/package-json-content (json-read-file snippy/package-json-location))
 
-;; Get path by language
-(setq snipPackage (assoc 'snippets (assoc 'contributes snippy/package-json-content)))
-(message snipPackage)
+(defun snippy/clean-version-string (version)
+  "Remove common semver prefixes like ^ or ~ from VERSION string."
+  (if (and (stringp version) (string-match "\\([0-9.]+\\)" version))
+      (match-string 1 version)
+    version))
 
-(dolist (current-alist snipPackage)
-  (when (string-equal "rust" (car (assoc 'language current-alist)))
-	(setq snippetPaths (append snippetPaths current-alist)) ;; Nem jó megoldás
-	))
+(defun snippy/get-engine-vscode-version ()
+  (snippy/clean-version-string (alist-get 'vscode (alist-get 'engines snippy/package-json-content))))
 
-;; (defun snippy/read-snippet-file (language)
-;;   "Read in snippet file"
-;;   (interactive "sString: ")
-;;   (setq snippy/snippet-content (json-read-file language))
-;;   )
+(defvar snippy/engine-vscode-version (snippy/get-engine-vscode-version)
+  "Current Engine vscode version")
+
+(defvar snippy/minimal-engine-vscode-version "1.11.0"
+  "The minimum version required for this package.")
+
+(defun snippy/check-engine-version()
+  (if (version<= snippy/minimal-engine-vscode-version snippy/engine-vscode-version)
+      (message "Okey %s" snippy/engine-vscode-version)
+    (message "Bady %s" snippy/engine-vscode-version))
+  )
+
+(snippy/check-engine-version)
