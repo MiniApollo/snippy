e27a27a63af646df7dc090af6729a55eb49333dd
Mark Surmann <surmannmark@gmail.com>
2026-01-28 21:34:48 +0100

    Snippy: Add basic language search

--- snippy.el
+++ snippy.el
@@ -14,11 +14,13 @@
 ;; TODO:
 ;; Only set vars for this package not change it globally
 
-;;(setq json-object-type 'alist) ;; To set type default is alist
+;;(setq-local json-object-type 'alist) ;; To set type default is alist
 (setq-local json-array-type 'list) ;; Default is vector e.g [Hello there my name is]
 
 (defvar snippy/package-json-location "./friendly-snippets/package.json" "Location of the package.json file")
 (defvar snippy/package-json-content (json-read-file snippy/package-json-location))
+
+;; Vscode engine check
 
 (defun snippy/clean-version-string (version)
   "Remove common semver prefixes like ^ or ~ from VERSION string."
@@ -41,4 +43,26 @@
     (message "Bady %s" snippy/engine-vscode-version))
   )
 
-(snippy/check-engine-version)
+;(snippy/check-engine-version)
+
+;; Read in by language
+;; Snippets
+(setq-local snippy/snippets-paths (alist-get 'snippets (alist-get 'contributes snippy/package-json-content)))
+;(message "%s" snippy/snippets-paths)
+
+(defun snippy/find-snippet-path (snippets-paths target-lang)
+  (let ((lang (alist-get 'language (aref snippets-paths 0))))
+    ;; You can now use 'lang' inside this block
+    (message "The first language is: %s" lang))
+  )
+
+;; (defun snippy/find-snippet-path (collection target-lang)
+;;   (let ((found (seq-find (lambda (item)
+;;                            (let ((langs (cdr (assoc 'language item))))
+;;                              (if (vectorp langs)
+;;                                  (seq-contains-p langs target-lang)
+;;                                (eq langs target-lang))))
+;;                          collection)))
+;;     (cdr (assoc 'path found))))
+
+(message "%s" (snippy/find-snippet-path snippy/snippets-paths 'c))
