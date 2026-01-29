664d271c9c07e60458b187e6da0f7dd13aa2ed18
Mark Surmann <surmannmark@gmail.com>
2026-01-29 20:05:48 +0100

    Snippy: Add Variable support

--- snippy.el
+++ snippy.el
@@ -11,7 +11,17 @@
 
 ;;; Code:
 
-;; TODO:
+;; TODO
+;; Variables
+;; Major mode loading
+;; Clean up for release
+
+;; Not very interesting
+;; Variable-Transform
+;; Placeholder-Transform
+;; Grammar
+
+
 ;; Only set vars for this package not change it globally
 ;;(setq-local json-object-type 'alist) ;; To set type default is alist
 ;(setq-local json-array-type 'list) ;; Default is vector e.g [Hello there my name is]
@@ -71,7 +81,7 @@
 ;(message "Result for C: %s" (snippy/get-all-paths-by-language snippy/snippets-paths "cpp"))
 ;(message "Result for Markdown: %s" (snippy/get-all-paths-by-language snippy/snippets-paths "rust"))
 
-(setq-local snippy/current-language "c")
+(setq-local snippy/current-language "css")
 (setq-local snippy/current-language-path (snippy/get-all-paths-by-language snippy/snippets-paths snippy/current-language))
 ;(message "%s" snippy/current-language-path)
 
@@ -100,13 +110,58 @@
                 (string= (cdr (assoc 'prefix snippet-data)) prefix)))
             snippets))
 
+;; Expand Snippet
 (defun snippy/expand-snippet-by-prefix (prefix)
   (interactive "sEnter snippet name: ")
+  ;; (unless (featurep 'yasnippet)
+  ;;   (user-error "Yasnippet is required for this function"))
+
   (message "%s" (snippy/find-snippet-by-prefix prefix snippy/merged-snippets))
   (snippy/expand-snippet (snippy/find-snippet-by-prefix prefix snippy/merged-snippets))
   )
 
 (require 'yasnippet)
+
+(defun snippy--get-variable-value (var-name)
+  "Resolves VS Code variables to their Emacs string values."
+  (let ((file-name (buffer-file-name)))
+    (pcase var-name
+      ("TM_SELECTED_TEXT" (if (use-region-p) (buffer-substring-no-properties (region-beginning) (region-end)) ""))
+      ("TM_CURRENT_LINE" (thing-at-point 'line t))
+      ("TM_CURRENT_WORD" (or (thing-at-point 'word t) ""))
+      ("TM_LINE_INDEX" (number-to-string (1- (line-number-at-pos))))
+      ("TM_LINE_NUMBER" (number-to-string (line-number-at-pos)))
+      ("TM_FILENAME" (if file-name (file-name-nondirectory file-name) "Untitled"))
+      ("TM_FILENAME_BASE" (if file-name (file-name-base file-name) "Untitled"))
+      ("TM_DIRECTORY" (if file-name (file-name-directory file-name) default-directory))
+      ("TM_DIRECTORY_BASE" (file-name-nondirectory (directory-file-name (if file-name (file-name-directory file-name) default-directory))))
+      ("TM_FILEPATH" (or file-name ""))
+      ("RELATIVE_FILEPATH" (if file-name (file-relative-name file-name) ""))
+      ("CLIPBOARD" (or (current-kill 0) ""))
+      ("WORKSPACE_NAME" (if (project-current) (file-name-nondirectory (directory-file-name (project-root (project-current)))) "No Workspace"))
+      ("WORKSPACE_FOLDER" (if (project-current) (project-root (project-current)) default-directory))
+
+      ;; Dates
+      ("CURRENT_YEAR" (format-time-string "%Y"))
+      ("CURRENT_YEAR_SHORT" (format-time-string "%y"))
+      ("CURRENT_MONTH" (format-time-string "%m"))
+      ("CURRENT_MONTH_NAME" (format-time-string "%B"))
+      ("CURRENT_MONTH_NAME_SHORT" (format-time-string "%b"))
+      ("CURRENT_DATE" (format-time-string "%d"))
+      ("CURRENT_DAY_NAME" (format-time-string "%A"))
+      ("CURRENT_DAY_NAME_SHORT" (format-time-string "%a"))
+      ("CURRENT_HOUR" (format-time-string "%H"))
+      ("CURRENT_MINUTE" (format-time-string "%M"))
+      ("CURRENT_SECOND" (format-time-string "%S"))
+      ("CURRENT_SECONDS_UNIX" (format-time-string "%s"))
+
+      ;; Randoms
+      ("RANDOM" (format "%06d" (random 1000000)))
+      ("RANDOM_HEX" (format "%06x" (random 16777215)))
+      ("UUID" (if (fboundp 'org-id-uuid) (org-id-uuid) (format "%06x%06x" (random 16777215) (random 16777215))))
+
+      ;; Fallback
+      (_ nil))))
 
 (defun snippy/expand-snippet (snippet)
   "Convert LSP-style choices to YASnippet elisp and expand, handling strings or vectors."
@@ -115,8 +170,9 @@
          (body-str (if (vectorp body-raw)
                        (mapconcat #'identity (append body-raw nil) "\n")
                      body-raw))
+         ;; Choices
          ;; Convert ${1|a,b|} to ${1:$$(yas-choose-value '("a" "b"))}
-         (convarted
+         (body-choices
           (replace-regexp-in-string
            "\\${\\([0-9]+\\)|\\([^|]+\\)|}"
            (lambda (match)
@@ -127,9 +183,17 @@
                       (lisp-list (format "'(%s)"
                                          (mapconcat #'prin1-to-string choices " "))))
                  (format "${%s:$$(yas-choose-value %s)}" index lisp-list))))
-           body-str
-           t t)
-          )
+           body-str t t))
+         ;; Variables
+         (final-body
+          (replace-regexp-in-string
+           "\\${\\([A-Z_]+\\)}"
+           (lambda (match)
+             (let ((var-name (match-string 1 match)))
+               ;; Call your helper function.
+               ;; We use (or ... var-name) as a fallback if the function returns nil.
+               (or (snippy--get-variable-value var-name) var-name)))
+           body-choices t t)))
 
-         )
-    (yas-expand-snippet convarted)))
+    (yas-expand-snippet final-body)))
+
