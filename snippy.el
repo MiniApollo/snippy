86f1d46c15806ea443f5b930e6439ac3d88afad3
Mark Surmann <surmannmark@gmail.com>
2026-02-06 16:10:05 +0100

    Snippy: Fix wrong version number

--- snippy.el
+++ snippy.el
@@ -7,7 +7,7 @@
 
 ;; Keywords: snippet
 ;; Package-Requires: ((emacs "26.1") (yasnippet "0.14.0"))
-;; Version: v1.1.1
+;; Version: 1.1.2
 ;; URL: https://github.com/MiniApollo/snippy
 
 ;; This file is not part of GNU Emacs.
