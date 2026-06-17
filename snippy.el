3aa0cef758414d2090e759daa85fe4bba31fa8a5
Mark Surmann <surmannmark@gmail.com>
2026-06-17 14:57:40 +0200

    Snippy: Rewrite minor modes WIP
    Not working

--- snippy.el
+++ snippy.el
@@ -525,47 +525,46 @@ Works even with an empty prefix/string."
 ;;; Minor Modes
 ;;; ============================================================================
 
+(defun snippy--enable ()
+  "Initialization logic for `snippy-minor-mode`."
+  (condition-case err
+      (progn
+        (unless snippy-package-json-content
+          (snippy-get-package-data))
+        (snippy-refresh-snippets)
+        (when (called-interactively-p 'any)
+          (snippy-check-engine-version)
+          (message "Snippy minor mode enabled")))
+    (error
+     (snippy-minor-mode -1)
+     (message "Snippy: Failed to initialize: %s" (error-message-string err)))))
+
+(defun snippy--disable ()
+  "Cleanup logic for `snippy-minor-mode`."
+  (setq snippy--buffer-language nil
+        snippy--merged-snippets nil)
+  (when (called-interactively-p 'any)
+    (message "Snippy minor mode disabled")))
+
 ;;;###autoload
 (define-minor-mode snippy-minor-mode
-  "Toggle snippy in the current buffer"
+  "Toggle snippy in the current buffer."
   :group 'snippy
   (if snippy-minor-mode
-      ;; Logic when the mode is TURNED ON
-      (condition-case err
-          (progn
-            (snippy-get-package-data)
-            (snippy-refresh-snippets)
-            (when (called-interactively-p 'any)
-              (progn (snippy-check-engine-version)
-                     (message "Snippy minor mode enabled in current buffer"))))
-        (error
-         (setq snippy-minor-mode nil)
-         (error "Failed to enable Snippy mode: %s" (error-message-string err))))
-    ;; Logic when the mode is TURNED OFF
-    (setq snippy--buffer-language nil
-          snippy--merged-snippets nil)
-    (when (called-interactively-p 'any)
-      (message "Snippy minor mode disabled in current buffer"))))
+      (snippy--enable)
+    (snippy--disable)))
 
+(defun snippy--turn-on ()
+  "Enable `snippy-minor-mode` only in file-visiting programming or text buffers."
+  (when (and buffer-file-name
+             (derived-mode-p 'prog-mode 'text-mode))
+    (snippy-minor-mode 1)))
 
 ;;;###autoload
 (define-globalized-minor-mode global-snippy-minor-mode
   snippy-minor-mode
   snippy--turn-on
-  :group 'snippy
-  (if global-snippy-minor-mode
-      (progn (snippy-check-engine-version)
-             (message "Global Snippy mode enabled"))
-    (message "Global Snippy mode disabled")))
-
-(defun snippy--turn-on ()
-  "Enable `snippy-minor-mode` only in file-visiting programming or text buffers."
-  (when (and (not (minibufferp))
-             ;; Only enable in "real" content buffers (Prog or Text)
-             (derived-mode-p 'prog-mode 'text-mode)
-             ;; Prevent infinite loops or redundant checks
-             (not snippy-minor-mode))
-    (snippy-minor-mode t)))
+  :group 'snippy)
 
 (provide 'snippy)
 ;;; snippy.el ends here
